dc5f279d368f6bea0306d2b4724210614a27c41c
victor <vlizanasanchez@gmail.com>
2021-12-09 10:19:36 +0100

    P3 empezada

---
+++ P3/app/createMongoDBFromPostgreSQLDB.py
@@ -0,0 +1,123 @@
+import os
+import sys, traceback
+import psycopg2
+import difflib
+import sqlalchemy as SQL
+import pymongo as pyMNG
+
+# Motor de sqlalchemy
+db_engine = SQL.create_engine("postgresql://alumnodb:alumnodb@localhost/si1",
+                              echo=False)
+
+
+# Conecta con la base de datos
+try:
+    db_conn = db_engine.connect()
+except Exception:
+    print("ERROR conecting database")
+    exit()
+
+
+# Datos de busqueda
+numPelis = 400
+pais = "Britain"
+
+
+# Querys
+query_topMovies = "SELECT MV.movieid AS movieid, MV.movietitle AS movietitle, " \
+                         "MV.year AS year, MC.country " \
+                  "FROM imdb_movies AS MV NATURAL JOIN " \
+                       "imdb_moviecountries AS MC " \
+                  "WHERE MC.country = '%%PAIS%%' " \
+                  "ORDER BY MV.year " \
+                  "LIMIT %%NUM%%;"
+
+query_movieGenres = "SELECT * " \
+                    "FROM imdb_moviegenres " \
+                    "WHERE movieid = %%ID%%;"
+
+query_movieDirectors = "SELECT DM.movieid, DR.directorname AS directorname " \
+                       "FROM imdb_directormovies AS DM NATURAL JOIN " \
+                            "imdb_directors AS DR " \
+                       "WHERE DM.movieid = %%ID%%;"
+
+query_movieActors = "SELECT AC.actorname AS actorname, AM.movieid " \
+                    "FROM imdb_actors AS AC NATURAL JOIN " \
+                         "imdb_actormovies AS AM " \
+                    "WHERE AM.movieid = %%ID%%;"
+
+
+# Obtiene las peliculas
+movies = []
+for movie in db_conn.execute(query_topMovies.replace(pais, "%%NUM%%").replace(numPelis, "%%NUM%%"):
+    if movie:
+        # Titulo sin la fecha
+        title = movie["movietitle"].split(" (")
+        title = title[0]
+
+        # Anio
+        ayear = movie["year"]
+
+        # Generos
+        genres = []
+        for genre in db_conn.execute(query_movieGenres.replace(movie["movieid"], "%%ID%%")):
+            if genre and genre not in genres:
+                genres.append(genre["genre"])
+
+        # Directores
+        directors = []
+        for director in db_conn.execute(query_movieDirectors.replace(movie["movieid"], "%%ID%%")):
+            if director and director not in directors:
+                directors.append(director["directorname"])
+
+        # Actores
+        actors = []
+        for actor in db_conn.execute(query_movieActors.replace(movie["movieid"], "%%ID%%")):
+            if actor[0] != "" and actor not in actors:
+                actors.append(actor["actorname"])
+
+        # Aniade la pelicula y su informacion a la lista
+        movies.append({"title"              : title,
+                       "genres"             : genre,
+                       "year"               : year,
+                       "directors"          : directors,
+                       "actors"             : actors,
+                       "most_related_movies": [],
+                       "related_movies"     : []})
+
+
+# Peliculas relacionadas y mas relacionadas
+for i in range(len(movies)):
+    m1 = movies[i]
+    for m2 in movies[i:]:
+        # Coincidencia de generos
+        c = (len(m1["genres"] & m2["genres"]) / len(m1["genres"] | m2["genres"])) * 100
+
+        if c == 100 and len(m1["genres"]) > 1 and len(m2["genres"]):
+            if len(m1["most_related_movies"]) < 10:
+                m1["most_related_movies"].append({"title": m2["title"],
+                                                  "year": m2["year"]})
+            if len(m2["most_related_movies"]) < 10:
+                m2["most_related_movies"].append({"title": m1["title"],
+                                                  "year": m1["year"]})
+        elif c >= 50:
+            if len(m1["related_movies"]) < 10:
+                m1["related_movies"].append({"title": m2["title"],
+                                             "year": m2["year"]})
+            if len(m2["related_movies"]) < 10:
+                m2["related_movies"].append({"title": m1["title"],
+                                             "year": m1["year"]})
+
+
+# Cierra la conexion con la base de datos
+db_conn.close()
+
+# Conecta con mongoDB
+mngClient = pyMNG.MongoClient("mongodb://localhost:27017/")
+
+# Obtiene la DB
+db = mngClient["si1"]
+
+# Obtiene y aniade el topUSA
+coleccion = db["topUK"]
+coleccion.insert_many(movies)
